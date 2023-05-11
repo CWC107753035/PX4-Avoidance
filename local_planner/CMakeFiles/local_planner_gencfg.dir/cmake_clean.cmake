fbe9a54724d4abb767382fc7a4168d4c25de63b4
jeff <papago111333@gmail.com>
2023-05-11 11:04:46 +0800

    test

---
+++ local_planner/CMakeFiles/local_planner_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/local_planner_gencfg"
+  "devel/include/local_planner/LocalPlannerNodeConfig.h"
+  "devel/lib/python3/dist-packages/local_planner/cfg/LocalPlannerNodeConfig.py"
+  "devel/share/local_planner/docs/LocalPlannerNodeConfig-usage.dox"
+  "devel/share/local_planner/docs/LocalPlannerNodeConfig.dox"
+  "devel/share/local_planner/docs/LocalPlannerNodeConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/local_planner_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
